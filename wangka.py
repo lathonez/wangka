e4c4eabd443712070802ef0a31e229cc629b7346
Stephen Hazleton <shazleto@gmail.com>
2017-04-15 23:26:05 +0000

    add code from lesson-three

--- wangka.py
+++ wangka.py
@@ -2,4 +2,22 @@ def run_wangka_program():
 
     print("Welcome to The Martu Wangka Numbers!")
 
+    number = int(input("Please enter a number: "))
+
+    print("The Martu Wangka says: " + get_wangka_number(number))
+
+def get_wangka_number(number):
+
+    mw_num = ""
+
+    if number == 1:
+
+        mw_num = "kuja"
+
+    else:
+
+        mw_num = "nope"
+
+    return mw_num
+
 run_wangka_program()
