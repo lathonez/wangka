e68f23fff616590359e8cfbd87bca6f8ea0396b3
Stephen Hazleton <shazleto@gmail.com>
2017-04-16 01:13:05 +0000

    adding code from lesson-nine

--- server.py
+++ server.py
@@ -1,15 +1,20 @@
 import web
 
 urls = (
+    '/translate', 'translate',
     '/(.*)', 'hello'
 )
 app = web.application(urls, globals())
 
 class hello:
     def GET(self, name):
-        if not name:
-            name = 'World'
-        return 'Hello, ' + name + '!'
+        render = web.template.render('.')
+        return render.index()
+        
+class translate:
+    def GET(self):
+        user_data = web.input()
+        return user_data.number
 
 if __name__ == "__main__":
     app.run()
